f8b837310c2ec818eafa5d6d51af4c9a596450d0
Mateusz Kajsztura <mkajsztura@davinci-studio.eu>
2021-01-08 06:34:54 +0100

    add custom web component

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,17 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store/root-store";
 import { BootstrapVue, IconsPlugin } from "bootstrap-vue";
+import { applyPolyfills, defineCustomElements } from 'stencil-starter/loader';
 
 Vue.config.productionTip = false;
+Vue.config.ignoredElements = [
+  'my-component',
+]
+
+applyPolyfills().then(() => {
+  defineCustomElements();
+});
+
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
 
